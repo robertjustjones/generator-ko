95a96dc08217d2861065f5c95b2fe07df059feda
Robert Jones <rjones@opdots.com>
2014-07-10 16:18:19 -0500

    Update viewmodel.js

--- component/templates/viewmodel.js
+++ component/templates/viewmodel.js
@@ -1,13 +1,12 @@
-define(['knockout', 'text!./<%= filename %>.html'], function(ko, templateMarkup) {
+exports.template = require( 'fs' ).readFileSync( __dirname + '/<%= filename %>.html', 'utf8');
 
-  function <%= viewModelClassName %>(params) {
-    this.message = ko.observable('Hello from the <%= name %> component!');
+var <%= viewModelClassName %> = exports.viewModel = function <%= viewModelClassName %>(params) {
+  var self = this;
+  self.message = ko.observable('Hello from the <%= name %> component!');
+  
+  
   }
 
-  // This runs when the component is torn down. Put here any logic necessary to clean up,
-  // for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
-  <%= viewModelClassName %>.prototype.dispose = function() { };
-  
-  return { viewModel: <%= viewModelClassName %>, template: templateMarkup };
-
-});
+// This runs when the component is torn down. Put here any logic necessary to clean up,
+// for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
+<%= viewModelClassName %>.prototype.dispose = function() { };
